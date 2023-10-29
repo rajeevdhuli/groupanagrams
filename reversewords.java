3f0468107792767232c873d5465fc5e591866597
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-10-29 10:06:13 +0530

    Update reversewords.java

--- reversewords.java
+++ reversewords.java
@@ -1,5 +1,7 @@
 Reverse words in a String
-
+    
+Using stack- O(n) time complexity, O(n) space complecity.
+    
 Stack<String> st = new Stack<>();
 String str = "";
 s = s + " ";
@@ -23,3 +25,34 @@ for(int i = 0; i < s.length(); i++)
   }
   ans+= st.pop();
   return ans;
+
+
+
+without stack , time - O(n) , space - O(1)
+
+
+class Solution {
+    public String reverseWords(String s){
+        s = " " + s;
+        int left = 0;
+        int right =  s.length() - 1;
+        String str = "";
+        String ans = "";
+        while(left <= right)
+        {
+            if(s.charAt(right) !=  ' ')
+            {
+                str = s.charAt(right) + str;
+            }
+            else
+            {
+                if(str != "")
+                   ans+=  str + " ";
+                str = "";
+            }
+            right--;
+        }
+        ans = ans.substring(0, ans.length() - 1);
+        return ans;
+    }
+}    
