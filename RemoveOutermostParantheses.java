95e60ff884c609af61525758fb4ed68c5d7e190e
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-10-31 11:07:11 +0530

    Update RemoveOutermostParantheses.java

--- RemoveOutermostParantheses.java
+++ RemoveOutermostParantheses.java
@@ -1,3 +1,5 @@
+USING STACK
+
 class Solution {
     public  String removeOuterParentheses(String s)
     {
@@ -24,4 +26,43 @@ class Solution {
         }
         return sb;
     }
+
+   WITHOUT STACK-
+
+    class Solution {
+    public  String removeOuterParentheses(String s)
+    {
+        //Stack<Character> st = new Stack<>();
+        int n = s.length();
+        String sb = "";
+        int i = 0;
+        int count = 0;
+        while( i < n)
+        {
+            char ch = s.charAt(i);
+            if(ch  == '(')
+            {
+                if(count >= 1)
+                {
+                   sb+= ch;
+                   count++;
+                }
+                else
+                   count++;
+            }
+            else if( ch == ')')
+            {
+                if(count > 1)
+                {
+                    sb+=ch;
+                    count--;
+                }
+                else
+                    count--;   
+            }
+            i++;
+        }
+        return sb;
+    }
+}
 }
