bf9791663548737fa509490ac9bcf4ae1a68cb23
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-01 21:28:55 +0530

    Update TrappingRainWater.java
    https://leetcode.com/problems/trapping-rain-water/description/

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -23,3 +23,31 @@ class Solution {
         return water;
     }
 }
+
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int water = 0;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = height[0];
+        right[n-1] = height[n-1];
+        for(int i = 1; i < n; i++)
+        {
+            left[i] = Math.max( height[i], left[i-1]);
+        }
+        for(int i = n - 2; i >= 0; i--)
+        {
+            right[i] = Math.max( height[i], right[i+1]);
+        }
+        for(int i = 0; i < n; i++)
+        {
+            int count;
+            count= Math.min(left[i], right[i]) - height[i];
+            if(count > 0)
+               water+= count;
+        }
+        return water;
+    }
+}
