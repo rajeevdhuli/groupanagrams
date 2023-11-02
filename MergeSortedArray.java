2bd8c82ed16548ffe74b1de2804afff45008289a
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-02 14:59:03 +0530

    Update MergeSortedArray.java
    https://www.codingninjas.com/studio/problems/merge-two-sorted-arrays-without-extra-space_6898839?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
    
    
    https://leetcode.com/problems/merge-sorted-array/description/

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,3 +1,5 @@
+CODING NINGAS-
+
 O(n+m) - 
 
 class Solution {
@@ -41,5 +43,70 @@ class Solution {
     }
 }
 
+Direct in place -
 
+public class Solution {
+    public static void mergeTwoSortedArraysWithoutExtraSpace(long []a, long []b){
+        // Write your code here.
+       int n = a.length;
+       int m = b.length;
+       int left = n-1;
+       int right = 0;
+       while(left >=0 && right < m)
+       {
+           if(a[left] > b[right])
+           {
+               long temp = b[right];
+               b[right] = a[left];
+               a[left] = temp;
+               left--; 
+               right++;
+           }
+           else  
+              break;
+       }
+       Arrays.sort(a);
+       Arrays.sort(b);
+    }
+}
+    
+
+LEETCODE VERSION PROBLEM-
+
+class Solution {
+    public void merge(int[] arr1, int m, int[] arr2, int n) 
+    {
+        int left = m - 1;
+        int right = n - 1;
+        int end = n + m - 1;
+        while(left >= 0 && right >= 0)
+        {
+            if( arr1[left] >= arr2[right])
+            {
+                arr1[end] = arr1[left];
+                end--;
+                left--;
+            }
+            else
+            {
+                arr1[end] = arr2[right];
+                end--;
+                right--;
+            }
+        }
+        while(right >= 0)
+        {
+            arr1[end] = arr2[right];
+            end--;
+            right--;
+        }
+        while(left >= 0)
+        {
+            arr1[end] = arr1[left];
+            end--;
+            left--;
+        }
+
+    }    
+    
 
