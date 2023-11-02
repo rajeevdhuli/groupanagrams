25a6e6c1c0e4a8efbe36faf478d74c43e6b89d97
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-02 11:07:11 +0530

    Update NICEsubarray.java
    https://leetcode.com/problems/count-number-of-nice-subarrays/description/

--- SubarraywithODDnumber.java
+++ SubarraywithODDnumber.java
@@ -26,3 +26,29 @@ public class Solution {
         return cnt;
     }
 }
+
+
+
+
+DIRECT NO NEED OF REPLACING-
+
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer,Integer> mp = new HashMap<>();
+        int cnt =0;
+        int sum =0;
+        for(int i =0; i < n; i++)
+        {
+            if(nums[i]%2 == 1)
+               sum++;
+            if(sum == k)
+               cnt++;
+            int rem = sum -k;
+            if(mp.containsKey(rem))
+               cnt+= mp.get(rem);
+            mp.put(sum,mp.getOrDefault(sum,0)+1);         
+        }
+        return cnt;
+    }
+}
