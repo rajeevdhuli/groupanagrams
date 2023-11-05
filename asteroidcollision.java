37fcc61c58aeeb2dabe10ca6a97b92637680582f
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-05 07:39:14 +0530

    Update asteroidcollision.java
    https://leetcode.com/problems/asteroid-collision/description/

--- asteroidcollision.java
+++ asteroidcollision.java
@@ -72,4 +72,37 @@ class Solution {
         return ans;
     }
 }  
-  
+
+
+
+OPTIMIZED-
+
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+        int n = asteroids.length;
+        for(int i:asteroids)
+        {
+            if(i > 0)
+               st.push(i);
+            else
+            {
+                while(!st.empty() && st.peek() > 0 && (st.peek() < Math.abs(i)))
+                    st.pop();
+                if(st.empty() || st.peek() < 0)
+                    st.push(i);
+                if(st.peek() > 0 && (st.peek() + i == 0))
+                    st.pop();        
+            }   
+        }
+        int len = st.size();
+        int[] ans = new int[len];
+        for(int i = len - 1; i >= 0 ;i--)
+        {
+            ans[i] = st.pop();
+        }
+        return ans;
+    }
+}    
+    
