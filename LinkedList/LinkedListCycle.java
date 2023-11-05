202f16cf123276c9b926bae0830f259ecc72c156
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-05 23:04:24 +0530

    Create LinkedListCycle.java
    https://leetcode.com/problems/linked-list-cycle/

---
+++ LinkedList/LinkedListCycle.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) 
+    {
+        if(head == null) 
+           return false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next!= null) 
+        {
+           fast = fast.next.next;
+           slow = slow.next;
+           if(fast == slow) 
+              return true;
+        }
+        return false;
+        //USING HASHSET
+       /* HashSet<ListNode> table =new HashSet<>();
+        while(head!=null)
+        {
+            if(table.contains(head))
+                return true;
+            table.add(head);    
+            head=head.next;    
+        }
+        return false;*/
+    }
+}
