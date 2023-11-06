78fa111a8394d3096cee1791abd63618b7c21008
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-06 11:12:30 +0530

    Update Palindrome.java
    https://leetcode.com/problems/palindrome-linked-list/

--- LinkedList/Palindrome.java
+++ LinkedList/Palindrome.java
@@ -1,4 +1,6 @@
-lass Solution {
+O(2N), O(N)-
+    
+Class Solution {
     public boolean isPalindrome(ListNode head) {
         Stack<Integer> st = new Stack<>();
         ListNode cur = head;
@@ -18,3 +20,41 @@ lass Solution {
 
     }
 }
+
+
+O(N) , O(N) -
+
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode cur = head;
+        ListNode slow = cur;
+        ListNode fast = cur;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode right = slow;
+        ListNode prev = null;
+        ListNode temp = right;
+        while(temp != null)
+        {
+            ListNode nex = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = nex;
+        }
+        ListNode left = head;
+        while(prev != null)
+        {
+            if(prev.val != left.val)
+               return false;
+            prev = prev.next;
+            left = left.next;
+
+        }
+        return true;
+    }
+}    
+    
