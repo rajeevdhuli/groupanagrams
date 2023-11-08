32853d3a8e6636d3cbeb5f940f6dbb82afbfd5b3
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-08 06:49:58 +0530

    Update RemoveDuplicateII.java
    https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/

--- LinkedList/RemoveDuplicateII.java
+++ LinkedList/RemoveDuplicateII.java
@@ -1,4 +1,4 @@
-
+SLOW AND FAST - 
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if(head==null)
@@ -20,3 +20,84 @@ class Solution {
         return dummy.next;
     }
 }
+
+
+JAVA TREEMAP -
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null)
+            return null;
+        TreeMap<Integer,Integer> mp = new TreeMap<>();
+        ListNode cur = head;
+        while(cur != null){
+            mp.put(cur.val, mp.getOrDefault(cur.val,0)+1);
+            cur = cur.next;
+        }
+        ListNode ans = new ListNode(-1);
+        ListNode res = ans;
+        for(int i : mp.keySet())
+        {
+            if(mp.get(i) == 1)
+            {
+                ListNode temp = new ListNode(i);
+                res.next = temp;
+                res = res.next;
+            }
+        }
+        ListNode fin = ans.next;
+        ans = null;
+        return fin;
+    }
+}
+
+
+C++ ORDERED MAP -
+
+ /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head== NULL)
+            return NULL;
+        map<int,int> mp;
+        ListNode* cur = head;
+        while(cur != NULL){
+            mp[cur->val]++;
+            cur = cur->next;
+        }
+        ListNode* ans = new ListNode(-1);
+        ListNode* res = ans;
+        for(auto i : mp)
+        {
+            if(i.second == 1)
+            {
+                ListNode* temp = new ListNode(i.first);
+                res->next = temp;
+                res = res->next;
+            }
+        }
+        ListNode* fin = ans->next;
+        delete ans;
+        return fin;
+    }
+};   
