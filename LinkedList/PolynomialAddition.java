f1ea8d276883a78172ffd9553241bc67dee8abdd
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-08 12:20:33 +0530

    Update PolynomialAddition.java
    https://practice.geeksforgeeks.org/problems/polynomial-addition/1?page=1&category=Linked%20List&difficulty=Medium&sortBy=submissions

--- LinkedList/PolynomialAddition.java
+++ LinkedList/PolynomialAddition.java
@@ -65,3 +65,70 @@ class Solution
     
     }
 }
+
+Without extra space - 
+class Solution
+{
+    public static Node addPolynomial(Node p1,Node p2)
+    {
+        //Add your code here.
+        Node dummy = new Node(-1,-1);
+        Node ans = dummy;
+        
+        while(p1 != null && p2 != null){
+            
+            if(p1.pow == p2.pow){
+                
+                p1.coeff = p1.coeff+p2.coeff;
+                ans.next = p1;
+                ans = ans.next;
+                p1 = p1.next;
+                p2 = p2.next;
+                
+            }
+            
+            else{
+                
+                if(p1.pow > p2.pow){
+                    
+                    ans.next = p1;
+                    ans = ans.next;
+                    p1= p1.next;
+                    
+                }
+                
+                else{
+                    
+                    ans.next = p2;
+                    ans = ans.next;
+                    p2=p2.next;
+                   
+                    
+                }
+            }
+            
+           
+        }
+        while(p1 != null){
+        
+            ans.next = p1;
+            ans = ans.next;
+            p1 = p1.next;
+            
+        }
+        
+        while(p2 != null){
+
+            ans.next = p2;
+            ans = ans.next;
+            p2 = p2.next;
+            
+        }
+        
+        Node res = dummy.next;
+        dummy = null;
+        
+        return res;
+    
+    }
+}
