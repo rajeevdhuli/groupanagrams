6add4f9f83ec9bcee853b1e779ed23a03a7c4c90
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-11 19:16:15 +0530

    Create SelectionSort.java
    https://www.codingninjas.com/studio/problems/selection-sort_624469?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for(int i = 0 ; i < n - 1; i++){
+            int min = i;
+            for(int j = i; j < n; j++){
+                if( arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        //Your code goes here
+    }
+}
