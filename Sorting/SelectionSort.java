4b786de02c722942abd9ebaf1304f2af4791401e
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-11 19:30:46 +0530

    Update SelectionSort.java
    https://www.codingninjas.com/studio/problems/selection-sort_624469?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -3,7 +3,7 @@ public class Solution {
         int n = arr.length;
         for(int i = 0 ; i < n - 1; i++){
             int min = i;
-            for(int j = i; j < n; j++){
+            for(int j = i+1; j < n; j++){
                 if( arr[j] < arr[min]){
                     min = j;
                 }
