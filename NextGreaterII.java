df1297e70e08bcd67d429eb053361264b506b5ef
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-13 21:23:45 +0530

    Update NextGreaterII.java
    https://leetcode.com/problems/next-greater-element-ii/description/

--- NextGreaterII.java
+++ NextGreaterII.java
@@ -17,3 +17,24 @@ class Solution {
         
     }
 }
+
+
+
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int n = nums.length;
+        int[] ans = new int[n];
+        int j = 0;
+            for(int i = 2* n - 1; i >= 0; i--){
+                while(!st.isEmpty() && st.peek() <= nums[i %n])
+                    st.pop();
+                ans[i%n] = (st.isEmpty()) ? (-1) : (st.peek());
+                st.push(nums[i%n]);
+            }
+
+        return ans;
+        
+    }
+}
