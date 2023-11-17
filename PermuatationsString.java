d6bac77a1da50d18c752e7c0260a0b05db87b8fb
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-17 15:54:58 +0530

    Update PermuatationsString.java
    https://leetcode.com/problems/permutation-in-string/description/

--- PermuatationsString.java
+++ PermuatationsString.java
@@ -49,6 +49,10 @@ class Solution {
 
 
 
+
+
+
+
 class Solution {
    public boolean checkInclusion(String s1, String s2) {
       int n = s1.length();
@@ -84,3 +88,66 @@ class Solution {
       return true; 
    }
 }
+
+
+
+
+
+
+
+
+PSEUDO CODE -
+
+    
+Function checkInclusion(s1, s2):
+    n = length of s1
+    m = length of s2
+
+    if n > m:
+        return false
+
+    freq1 = array of size 26 initialized with zeros
+    freq2 = array of size 26 initialized with zeros
+
+    // Count frequencies of characters in the first n characters of s2 and s1
+    for i from 0 to n-1:
+        freq1[s1[i] - 'a']++
+        freq2[s2[i] - 'a']++
+
+    cnt = 0
+
+    // Compare frequencies of characters and count how many are same
+    for i from 0 to 25:
+        if freq1[i] == freq2[i]:
+            cnt++
+
+    // Slide a window through s2 and update frequencies accordingly
+    for i from n to m-1:
+        r = s2[i] - 'a'
+        l = s2[i - n] - 'a'
+
+        // If all frequencies match, return true
+        if cnt == 26:
+            return true
+
+        freq2[r]++
+
+        // Update cnt based on the changes in frequencies
+        //if new element adding on s2 is same as s2 then ++;
+        // else --;
+        if freq1[r] == freq2[r]:
+            cnt++
+        else if freq2[r] == (freq1[r] + 1):
+            cnt--
+
+        freq2[l]--
+
+        // Update cnt based on the changes in frequencies
+        // if after removing they become same like 'e' in s1 is 0 , and after removing 'e' in s2 it becomes 0 then you increment count, 
+        //else if elemnt was present in s1 and now you are removing it then you decremnt it .
+        if freq1[l] == freq2[l]:
+            cnt++
+        else if freq2[l] == (freq1[l] - 1):
+            cnt--
+
+    return (cnt == 26)
