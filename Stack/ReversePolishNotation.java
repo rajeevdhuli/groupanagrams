e01e8229d53ed5c4bfa11ef90d7637f5f9f7834e
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-20 13:30:04 +0530

    Create ReversePolishNotation.java
    https://leetcode.com/problems/evaluate-reverse-polish-notation/description/

---
+++ Stack/ReversePolishNotation.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        int n = tokens.length;
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < n; i++){
+
+            if(isDigit(tokens[i]) == true) {
+                st.push(Integer.parseInt(tokens[i]));
+            }
+
+            else if(isOperator(tokens[i]) == true){
+                int c1 = st.pop();
+                int c2 = st.pop();
+                int res = perform(tokens[i],c1,c2);
+                st.push(res);
+            }
+            
+        }
+        return st.pop();
+    }
+
+    public static int perform(String s,int i,int j){
+        int ans = 1;
+        switch (s)
+        {
+            case "+":
+            ans = i + j;
+            break;
+            case "-":
+            ans = j - i;
+            break;
+            case "*":
+            ans = i * j;
+            break;
+            case "/":
+            ans = j / i;
+            break;
+            default:
+            ans = -1;
+            break;
+        }
+        return ans;
+    }
+
+    boolean isOperator(String s){
+        if(s.equals("+") || s.equals("-") || s.equals("/") || s.equals("*"))
+            return true;
+        return false;    
+    }
+
+    boolean isDigit(String str) {
+        try{
+            int number = Integer.parseInt(str);
+                if(number >= -200 && number <= 200);
+                    return true;
+        }catch(NumberFormatException e){
+            return false;
+        } 
+    }
+}
