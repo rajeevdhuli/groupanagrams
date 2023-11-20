25e52a339310d93f1a7d17ba90d3a59a55cae979
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-20 13:50:14 +0530

    Create MaxNestingdepth.java
    https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/

---
+++ Stack/MaxNestingdepth.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxDepth(String s) 
+    {
+        //Stack<Character> st = new Stack<>();
+        int n = s.length();
+        int i = 0;
+        int cnt = 0;
+        int maxcnt = 0;
+        while( i < n )
+        {
+            char ch = s.charAt(i);
+            if(ch == '(')
+            {
+                //st.push(ch);
+                cnt++;
+                maxcnt  = Math.max(maxcnt, cnt);
+            }
+            else if (ch == ')')
+            {
+                //st.pop();
+                cnt--;
+            }
+            i++;
+        }
+        return maxcnt;
+    }
+}
