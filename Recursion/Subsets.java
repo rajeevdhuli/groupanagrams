7a18b3e8a32293d8f6b16378007bbeb6f6bef43a
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-24 15:42:52 +0530

    Update Subsets.java
    https://leetcode.com/problems/subsets/description/

--- Recursion/Subsets.java
+++ Recursion/Subsets.java
@@ -19,3 +19,54 @@ class Solution {
          
     }
 }
+
+
+          []
+        /    \
+  [1]           []
+ /   \       /     \
+[1,2]  [1]  [2]    []
+/  \   / \   / \   / \
+[1,2,3] [1,3][2,3][3] []
+
+
+
+Approach 2 - 
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    //Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+
+    list.add(new ArrayList<>(tempList));
+    //System.out.println("Enter "+start+" "+list);
+     
+    for(int i = start; i < nums.length; i++){
+        tempList.add(nums[i]);
+        //System.out.println("1st "+i+" "+tempList);
+
+        backtrack(list, tempList, nums, i + 1);
+       
+        //System.out.println("2nd "+i+" "+tempList);
+        tempList.remove(tempList.size() - 1);
+    }
+}
+}
+
+
+
+                            [] (empty set)
+                   /                     \
+           [1]                                 []
+        /        \                          /       \
+   [1, 2]       [1]                  [2]         []
+   /    \       /    \              /    \      /     \
+[1,2,3] [1,2] [1,3] [1]         [2,3] [2]  [3]      []
+
+
+
