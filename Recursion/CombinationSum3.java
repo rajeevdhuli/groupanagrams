6f6f5846d5fe0b6580e1057fe7b770caaa12ce5c
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-27 15:25:50 +0530

    Update CombinationSum3.java
    https://leetcode.com/problems/combination-sum-iii/description/

--- Recursion/CombinationSum3.java
+++ Recursion/CombinationSum3.java
@@ -24,7 +24,8 @@ class Solution {
 }
 
 
-
+USING FOR LOOP -
+    
 
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
@@ -44,6 +45,35 @@ class Solution {
         for(int i = start; i < nums.length; i++){
         bag.add(nums[i]);
         helper(trolly,bag,nums,target,k,i+1,sum+nums[i]);
+        bag.remove(bag.size()-1);
+        }
+    }
+}
+
+
+
+
+
+
+
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        List<List<Integer>> trolly = new ArrayList<>();
+        helper(trolly,new ArrayList<>(),1,n,k,0);
+        return trolly;
+    }
+    public void helper(List<List<Integer>> trolly,List<Integer> bag,int start,int target,int k,int sum){
+        if(sum == target && bag.size() == k){
+            trolly.add(new ArrayList<>(bag));
+            return;
+        }
+        if(sum > target || bag.size() > k){
+            return;
+        }
+        for(int i = start; i <= 9; i++){
+        bag.add(i);
+        helper(trolly,bag,i+1,target,k,sum+i);
         bag.remove(bag.size()-1);
         }
     }
