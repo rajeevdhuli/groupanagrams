3980354dadc2328cf33658157113f78a9a5b1401
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-27 12:01:57 +0530

    Update CombinationSum.java
    https://leetcode.com/problems/combination-sum/description/

--- Recursion/CombinationSum.java
+++ Recursion/CombinationSum.java
@@ -28,3 +28,29 @@ class Solution {
         helper(trolly,bag,can,target,i+1,sum);
     }
 }
+
+
+
+
+
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> trolly = new ArrayList<>();
+        helper(trolly,new ArrayList<>(),candidates,target,0,0);
+        return trolly;
+    }
+    public void helper(List<List<Integer>> trolly, List<Integer> bag,int[] can, int target, int start,int sum){
+        if(sum > target)
+           return;
+        if(sum == target)
+           trolly.add(new ArrayList<>(bag));
+        for(int i = start; i < can.length; i++){
+            // if(i > start && can[i] == can[i-1])
+            //     continue;
+            bag.add(can[i]);
+            helper(trolly,bag,can,target,i,sum+can[i]);
+            bag.remove(bag.size()-1);
+        }
+    }
+}
