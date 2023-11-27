9e7d3628a929d2a56a756dd4ec79c2c89206dff3
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-27 19:40:18 +0530

    Update PhoneLetterCombination.java
    https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/

--- Recursion/PhoneLetterCombination.java
+++ Recursion/PhoneLetterCombination.java
@@ -18,3 +18,29 @@ class Solution {
         }
     }
 }
+
+
+
+
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        //String[] keys = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        char[][] keys = {{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
+        List<String> trolly = new ArrayList<>();
+        if(digits == null || digits.length() == 0) 
+            return trolly;
+        helper(trolly,keys,digits,"",0);
+        return trolly;
+    }
+    public void helper(List<String> trolly,char[][] keys,String digits,String res,int start){
+        if(start == digits.length()){
+            trolly.add(res);
+            return;
+        }
+        char[] letters = keys[digits.charAt(start)-'0'];
+        for(int i = 0;i < letters.length; i++){
+            helper(trolly,keys,digits,res+letters[i],start+1);
+        }
+    }
+}
