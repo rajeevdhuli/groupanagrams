8f5ea3bc4647c6ba34c581f125e24b1eacfd6d7c
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-27 11:38:27 +0530

    Create CombinationSum.java
    https://leetcode.com/problems/combination-sum/description/

---
+++ Recursion/CombinationSum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> trolly = new ArrayList<>();
+        helper(trolly,new ArrayList<>(),candidates,target,0,0);
+        return trolly;
+    }
+    public void helper(List<List<Integer>> trolly, List<Integer> bag,int[] can, int target, int i,int sum){
+        if(sum == target){
+            List<Integer> newBag = new ArrayList<>();
+            newBag.addAll(bag);
+            trolly.add(newBag);
+            return;
+        }
+        if(sum > target){
+            //System.out.println("Greater "+ i +" "+bag+" "+sum);
+            return;
+        }
+        if(i == can.length) {
+            return;
+        }
+        //System.out.println("befor 1st "+ i +" "+bag+" "+sum);
+        bag.add(can[i]);
+        //System.out.println("after 1st "+ i +" "+bag+" "+sum);
+        helper(trolly,bag,can,target,i,sum+can[i]);
+        //System.out.println("befor 2nd "+ i +" "+bag+" "+sum);
+        bag.remove(bag.size()-1);
+        //System.out.println("after 2nd "+ i +" "+bag+" "+sum);
+        helper(trolly,bag,can,target,i+1,sum);
+    }
+}
