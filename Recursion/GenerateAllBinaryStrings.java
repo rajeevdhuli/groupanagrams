41af36fb5951d5a306ceaa4aa84944c0073fd041
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-28 09:46:05 +0530

    Create GenerateAllBinaryStrings.java
    https://www.codingninjas.com/studio/problems/-binary-strings-with-no-consecutive-1s._893001?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION

---
+++ Recursion/GenerateAllBinaryStrings.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.*;
+
+public class Solution {
+    public static void helper(List<String> trolly,int prev,int N, StringBuilder sb,int start){
+        //System.out.println(start+" "+sb);
+        if(sb.length() == N){
+           trolly.add(new String(sb));
+           return;
+        }
+        sb.append("0");
+        helper(trolly,0,N,sb,start+1);
+        sb.deleteCharAt(sb.length()-1);
+        if(prev != 1){
+            sb.append("1");
+            helper(trolly,1,N,sb,start+1);
+            sb.deleteCharAt(sb.length()-1);
+        } 
+
+
+       
+    }
+    public static List< String > generateString(int N) {
+        // Write your code here.
+        List<String> trolly = new ArrayList<>();
+        helper(trolly,0,N,new StringBuilder(),0);
+        return trolly;
+    }
+}
