ccfc10fb80a4d92366ce7947fc006d62f811ade6
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-11-30 14:59:41 +0530

    Update MergeIntervals.cpp
    https://leetcode.com/problems/merge-intervals/description/?envType=study-plan-v2&envId=top-interview-150

--- MergeIntervals.cpp
+++ MergeIntervals.cpp
@@ -22,3 +22,33 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        int n = intervals.size();
+        vector<vector<int>> v;
+        for(int i = 0; i < n; i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            for(int j = i + 1; j < n; j++){
+                if(end >= intervals[j][0]){
+                    end = max(end,intervals[j][1]);
+                    i++;
+                }
+                else{
+                    break;
+                }
+            }
+            v.push_back({start,end});
+        }
+        return v;  
+    }
+};
