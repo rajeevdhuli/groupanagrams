b9f875947dfd6ff32745f5243c922aa4dfd28dbc
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-03 07:46:43 +0530

    Create MinTimePoints.java
    https://leetcode.com/problems/minimum-time-visiting-all-points/?envType=daily-question&envId=2023-12-03

---
+++ 2DArraysMatrix/MinTimePoints.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int distance = 0;
+        for (int i = 0; i < points.length - 1; i++)
+        {
+            distance += Math.max(Math.abs(points[i + 1][0] - points[i][0]), Math.abs(points[i + 1][1] - points[i][1]));
+        }
+        return distance;
+    }
+}
