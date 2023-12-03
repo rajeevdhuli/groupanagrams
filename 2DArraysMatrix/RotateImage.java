f29b42ec186ec2aa3e982dfbce6d89a70ddc13cd
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-03 11:00:00 +0530

    Update RotateImage.java
    https://leetcode.com/problems/rotate-image/description/

--- 2DArraysMatrix/RotateImage.java
+++ 2DArraysMatrix/RotateImage.java
@@ -15,3 +15,30 @@ class Solution {
         }
     }
 }
+
+Transpose and reverse each row - 
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i = 0; i < m ; i++){
+            for(int j = i ; j < m ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j]= matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0 ; i < m ; i++){
+            int j = 0;
+            int k = m - 1;
+            while(j< n && j < k){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+                k--;
+                j++;
+            }
+        }
+    }
+}
