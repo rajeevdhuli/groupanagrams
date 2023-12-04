ce51690cad8f9ea569fc99c496388e323bbe1bac
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-04 16:34:10 +0530

    Update InsertInterval.cpp
    https://leetcode.com/problems/insert-interval/description/?envType=study-plan-v2&envId=top-interview-150

--- InsertInterval.cpp
+++ InsertInterval.cpp
@@ -24,3 +24,38 @@ public:
        return v;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> v  ;
+        int i = 0 ;
+        int n = intervals.size();
+        while(i < n && intervals[i][1] < newInterval[0]){
+            v.push_back(intervals[i]);
+            i++;
+        }
+        while(i < n && newInterval[1] >= intervals[i][0]){
+            newInterval[0] = min(newInterval[0],intervals[i][0]);
+            newInterval[1] = max(newInterval[1],intervals[i][1]);
+            i++;
+        }
+        v.push_back(newInterval);
+        while( i < n ){
+            v.push_back(intervals[i]);
+            i++;
+        }
+        return v;
+    }
+};
