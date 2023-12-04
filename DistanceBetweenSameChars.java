995f78494452b3759fe826bc19078fd5cc9552b5
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-04 11:08:06 +0530

    Update DistanceBetweenSameChars.java
    https://leetcode.com/problems/check-distances-between-same-letters/description/

--- DistanceBetweenSameChars.java
+++ DistanceBetweenSameChars.java
@@ -15,3 +15,28 @@ class Solution {
         return true;
     }
 }
+
+
+
+
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        int[] freq = new int[26];
+        boolean[] used = new boolean[26];
+        for(int i = 0; i < s.length(); i++){
+            char c=  s.charAt(i);
+            if(freq[c-'a'] == 0){
+                freq[c-'a'] = i+1;
+            }else{
+                used[c-'a'] = true;
+                int res = i - freq[c-'a'] ;
+                freq[c- 'a'] = res;
+            }
+        }
+        for(int i = 0 ;i <26; i++){
+            if(used[i] && distance[i] != freq[i])
+               return false;
+        }
+        return true;
+    }
+}
