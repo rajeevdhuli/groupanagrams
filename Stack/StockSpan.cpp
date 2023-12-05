a4dc48adf09b0473dee8a6e27f23fc0b49745aad
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-05 10:02:53 +0530

    Update StockSpan.cpp
    https://www.codingninjas.com/studio/problems/stock-span_5243295?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM

--- Stack/StockSpan.cpp
+++ Stack/StockSpan.cpp
@@ -19,33 +19,49 @@ O(N^2)-
 
 O(N) STACK -
 
-public static int[] calculateSpan(int price[], int n)
-    {
-        int S[] = new int[n];
-        Stack<Integer> st=new Stack<>();
-        st.push(0);
-        
-        //span value of first day is always 1.
-        S[0]=1;
-        
-        for(int i=1;i<n;i++) 
-        { 
-            //we pop elements from the stack till price at top of stack 
-            //is less than or equal to current price.
-            while(!st.isEmpty() && price[i]>=price[st.peek()])
-            {
+import java.util.*;
+public class Solution {
+    public static int[] findStockSpans(int []prices) {
+        // Write your code here.
+        int n = prices.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0 ; i < n ; i++){
+            while(!st.isEmpty() && prices[i] > prices[st.peek()]){
                 st.pop();
             }
-
-              //if stack becomes empty, then price[i] is greater than all 
-            //elements on left of it in list so span is i+1.
-            //Else price[i] is greater than elements after value at top of stack.
-            S[i]=st.isEmpty()?i+1:i-st.peek();
-            
-            //pushing this element to stack.
+            if(!st.isEmpty())
+               ans[i] =i - st.peek() ;
+            else{
+                ans[i] = i + 1;
+            }   
             st.push(i);
-            
+
         }
-        //returning the list.
-        return S;
+        return ans;
     }
+}
+
+
+Optimized - 
+
+
+import java.util.*;
+public class Solution {
+    public static int[] findStockSpans(int []prices) {
+        // Write your code here.
+        int n = prices.length;
+        int[] ans = new int[n];
+        Stack<int[]> st = new Stack<>();
+        for(int i = 0 ; i < n ; i++){
+            int span = 1;
+            while(!st.isEmpty() && prices[i] >st.peek()[0]){
+                span+= st.peek()[1];
+                st.pop();
+            }
+            ans[i] = span;
+            st.push(new int[]{prices[i],span});
+        }
+        return ans;
+    }
+}    
