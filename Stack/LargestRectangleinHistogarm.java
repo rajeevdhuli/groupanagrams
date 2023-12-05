95bf6729376dc7eb59c7ec1283825fcd76889bfa
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-05 16:54:10 +0530

    Update LargestRectangleinHistogarm.java
    https://leetcode.com/problems/largest-rectangle-in-histogram/description/

--- Stack/LargestRectangleinHistogarm.java
+++ Stack/LargestRectangleinHistogarm.java
@@ -55,3 +55,30 @@ class Solution {
         return maxArea;
     }
 }
+
+
+
+
+
+
+O(N),O(N) -
+
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Stack<Integer> st = new Stack<>();
+        int maxArea = 0 ;
+        for(int i =0 ; i <= n; i++){
+            while(!st.isEmpty() && (i == n ||  heights[i] <= heights[st.peek()])){
+                int height = heights[st.peek()];
+                st.pop();
+                int width = (st.isEmpty()) ? i : (i - st.peek() - 1);
+                maxArea = Math.max(maxArea, height * width);
+            }
+            st.push(i);
+        }
+        return maxArea;
+
+    }
+}    
