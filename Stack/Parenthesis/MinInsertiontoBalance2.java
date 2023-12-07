f2f1ad280e700add335c5911b9c67ddeaae892d6
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-07 19:30:47 +0530

    Update MinInsertiontoBalance2.java
    https://leetcode.com/problems/minimum-insertions-to-balance-a-parentheses-string/description/

--- Stack/Parenthesis/MinInsertiontoBalance2.java
+++ Stack/Parenthesis/MinInsertiontoBalance2.java
@@ -10,21 +10,27 @@ class Solution {
                 st.push(ch);
             }else if (ch == ')'){
                 if(i < n - 1  && s.charAt(i+1) == ')' && !st.isEmpty()){
+                    // ()) in this case you dont have to add anything
                     st.pop();
                     i++;
-                }
-                else if(st.isEmpty() && i < n - 1 && s.charAt(i+1) == ')'){ 
+                }else if(!st.isEmpty()  && st.peek() == '('){
+                   // () in this case you have to one more')' to balance
+                   cnt++;
+                   st.pop();
+                }else if(st.isEmpty() && i < n - 1 && s.charAt(i+1) == ')'){ 
+                    // )) in this case you have to add one more '(' to balance
                     cnt++;
                     i++;
                 }else if(st.isEmpty() ){
-                  cnt+= 2;
-                }else if(!st.isEmpty()  && st.peek() == '('){
-                   cnt++;
-                   st.pop();
+                    // ) in this case you have to add one opening '(' and one closing ')'
+                    // therefore you increment count by 2
+                    cnt+= 2;
                 }
             }
         }
-        if(!st.isEmpty() )
+        if(!st.isEmpty())
+        // (((( if all are opening brackets or if string is ending with opening brackets
+        //you have to add twp closing brackets for each (2*st.size()) to balance it.
             cnt+= 2*st.size();
         return cnt;
     }
