fc9f7f5a73240c537eb1aab1dccd49093517b468
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-08 07:09:31 +0530

    Update ConstructStringfromBinaryTree.java
    https://leetcode.com/problems/construct-string-from-binary-tree/description/?envType=daily-question&envId=2023-12-08

--- BinaryTree/ConstructStringfromBinaryTree.java
+++ BinaryTree/ConstructStringfromBinaryTree.java
@@ -42,3 +42,57 @@ class Solution {
         }
     }
 }
+
+
+
+
+C++ CODE -------------------------------------------------------------------------------------------------------
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+                if (root == nullptr) {
+            return "";
+        }
+        string result;
+        helper(root, result);
+        return result;
+    }
+    
+private:
+    void helper(TreeNode* root, string& result) {
+        result += to_string(root->val);
+        if (root->left == nullptr && root->right == nullptr) {
+            return;
+        }
+        if (root->left != nullptr) {
+            result += '(';
+            helper(root->left, result);
+            result += ')';
+        }
+        if (root->right != nullptr) {
+            if (root->left == nullptr) {
+                result += "()";
+            }
+            result += '(';
+            helper(root->right, result);
+            result += ')';
+        }
+    }  
+};
+
+
+
