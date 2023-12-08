94c4ec649a790091baef69e0cda9695a6570a2be
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-08 09:38:24 +0530

    Create PreOrderTraversal.java
    https://leetcode.com/problems/binary-tree-preorder-traversal/description/

---
+++ BinaryTree/PreOrderTraversal.java
@@ -0,0 +1,29 @@
+RECURSIVE O(N) , O(N) -
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  List<Integer> preOrder = new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+      
+      if(root == null)
+        return preOrder;
+      preOrder.add(root.val);
+      preorderTraversal(root.left);
+      preorderTraversal(root.right);
+      return preOrder; 
+    }
+}
