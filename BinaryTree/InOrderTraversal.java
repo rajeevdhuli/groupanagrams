5083b804aa2d4405b51a8f5d0c3a6af70795391c
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-08 09:39:04 +0530

    Create InOrderTraversal.java
    https://leetcode.com/problems/binary-tree-inorder-traversal/description/

---
+++ BinaryTree/InOrderTraversal.java
@@ -0,0 +1,28 @@
+RECURSIVE O(N),O(N) -
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> inOrder = new ArrayList<>(); 
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null){
+          return inOrder;
+        }
+        inorderTraversal(root.left);
+        inOrder.add(root.val);
+        inorderTraversal(root.right);
+        return inOrder;
+    }
+}
