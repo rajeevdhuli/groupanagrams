1d803c75015e19c0bed52fd29f8fad5fa96245ae
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-08 10:37:53 +0530

    Update PreOrderTraversal.java
    https://leetcode.com/problems/binary-tree-preorder-traversal/submissions/

--- BinaryTree/PreOrderTraversal.java
+++ BinaryTree/PreOrderTraversal.java
@@ -27,3 +27,100 @@ class Solution {
       return preOrder; 
     }
 }
+
+
+
+
+ITERATIVE O(N),O(N) ------------------------------------------------------------------------------------------------------------------------
+
+
+
+  
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {  
+        List<Integer> preOrder = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root != null)
+           st.push(root);
+        while(root != null){
+            preOrder.add(st.peek().val);
+            st.pop();
+            if(root.right!= null){
+                st.push(root.right);
+            }
+            if(root.left!= null){
+                st.push(root.left);
+            }
+            if(root.left != null){
+                root = root.left;
+            }else if(root.right != null){
+                root = root.right;
+            }else if(!st.isEmpty()){
+              root =  st.peek();
+            }else{
+              break;
+            }
+        }
+        return preOrder;
+    }
+}
+
+
+
+
+MORE OPTIMIZED --
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {  
+        List<Integer> preOrder = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root != null)
+           st.push(root);
+        while(!st.isEmpty()){
+            TreeNode cur = st.peek();
+            preOrder.add(cur.val);
+            st.pop();
+            if(cur.right!= null){
+              st.push(cur.right);
+            }
+            if(cur.left!= null){
+              st.push(cur.left);
+            }
+        }
+        return preOrder;
+    }
+}  
+  
