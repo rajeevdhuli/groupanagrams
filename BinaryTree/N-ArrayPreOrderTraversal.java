97f4cdab59ce95a540a4995ecd7672e419b74b4c
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-09 14:59:49 +0530

    Update N-ArrayPreOrderTraversal.java
    https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/

--- BinaryTree/N-ArrayPreOrderTraversal.java
+++ BinaryTree/N-ArrayPreOrderTraversal.java
@@ -33,3 +33,46 @@ class Solution {
         }
     }
 }
+
+
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> preOrder = new ArrayList<>();
+        Stack<Node> st = new Stack<>();
+        if(root == null){
+            return preOrder;
+        }
+        st.push(root);
+        while(st.size() > 0){
+            root = st.peek();
+            preOrder.add(root.val);
+            st.pop();
+            int size = root.children.size();
+            for(int i = size - 1; i >= 0; i--){
+                st.push(root.children.get(i));
+            }
+        }
+        return preOrder;
+    }
+}
