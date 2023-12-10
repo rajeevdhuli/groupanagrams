677072dfa306ebc638b238384a138d9e72a2b929
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-10 15:24:20 +0530

    Create SymmetricTree.java
    https://leetcode.com/problems/symmetric-tree/description/

---
+++ BinaryTree/SymmetricTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        TreeNode root1 = root.left;
+        TreeNode root2 = root.right;
+        return helper(root1,root2);
+    }
+    public boolean  helper(TreeNode root1,TreeNode root2){
+        if(root1 == null && root2 == null)
+            return true;
+        if(root1 == null || root2 == null || root1.val != root2.val)
+            return false;
+        if(root1.val == root2.val){
+            boolean left = helper(root1.left,root2.right);
+            boolean right = helper(root1.right,root2.left);
+            if(left && right){
+                return true;
+            }
+        }
+        return false;
+    }
+}
