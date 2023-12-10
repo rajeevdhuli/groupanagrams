03e930fdd88865df0daf96a04ffafdd93680b2af
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-10 15:24:44 +0530

    Update SameTree.java
    https://leetcode.com/problems/same-tree/description/

--- BinaryTree/SameTree.java
+++ BinaryTree/SameTree.java
@@ -7,3 +7,41 @@ class Solution {
         return ((p.val == q.val)&&(isSameTree(p.left,q.left))&&(isSameTree(p.right,q.right)));       
     }
 }
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return helper(p,q);      
+    }
+    public boolean helper(TreeNode p, TreeNode q){
+        if(p == null && q == null)
+           return true;
+        if(p == null || q == null)
+           return false;
+        if(p.val != q.val)
+           return false;
+        if(p.val == q.val){
+            boolean left = helper(p.left,q.left);
+            boolean right = helper(p.right,q.right);
+            if(left && right)
+               return true;
+        }
+        return false;         
+    }
+}
