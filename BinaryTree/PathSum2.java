9bf1232ccfd6ace1705e9b1445bb5517ffa17eb7
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-12 12:31:47 +0530

    Update PathSum2.java
    https://leetcode.com/problems/path-sum-ii/description/

--- BinaryTree/PathSum2.java
+++ BinaryTree/PathSum2.java
@@ -20,3 +20,48 @@ class Solution {
         bag.remove(bag.size()-1);   
     }
 }
+
+
+
+
+class threePair{
+    TreeNode root;
+    int targetSum;
+    List<Integer> path;
+    threePair(TreeNode root,int targetSum,List<Integer> path){
+        this.root = root;
+        this.targetSum = targetSum;
+        this.path = path;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<threePair> queue = new LinkedList<>();
+        if(root == null){
+           return res;
+        }
+        queue.offer(new threePair(root, root.val, new ArrayList<>(Arrays.asList((root.val)))));
+        while(!queue.isEmpty()) {
+            threePair temp = queue.poll();
+            TreeNode node = temp.root;
+            int sum = temp.targetSum;
+            List<Integer> list = temp.path;
+            if(node.left == null && node.right == null && sum == targetSum){
+                res.add(list);
+            }
+            if(node.left != null){
+                List<Integer> leftList = new ArrayList<>(list);
+                leftList.add(node.left.val);
+                queue.offer(new threePair(node.left,node.left.val+sum,leftList));
+            }
+            if(node.right != null){
+                List<Integer> rightList = new ArrayList<>(list);
+                rightList.add(node.right.val);
+                queue.offer(new threePair(node.right,node.right.val+sum,rightList));
+            }
+        }
+        return res;
+    }
+}
