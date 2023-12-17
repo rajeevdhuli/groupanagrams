a71bbebd86c28e2cb062f6ae1df1ae8eb851422f
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-17 07:02:49 +0530

    Create KthLargestArray,.java
    https://leetcode.com/problems/kth-largest-element-in-an-array/description/

---
+++ Heap/KthLargestArray,.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findKthLargest(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<>(){
+            public int compare(Integer a, Integer b){
+                return b - a;
+            }
+        });
+        for(int i = 0; i < arr.length; i++){
+            pq.add(arr[i]);
+        }
+        for(int i = 1 ; i < k ; i++){
+            pq.poll();
+        }
+        return pq.poll();
+    }
+}
