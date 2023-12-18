e718d7318fea97142ec3a66f84e41fd4b5546e77
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-18 07:39:21 +0530

    Create InsertBInaryTree.java
    https://leetcode.com/problems/insert-into-a-binary-search-tree/description/

---
+++ BinaryTree/BST/InsertBInaryTree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null){
+            TreeNode node = new TreeNode(val);
+            return node;
+        }
+        else if(root.val < val){
+            root.right  = insertIntoBST(root.right,val);
+        }else if(root.val > val){
+            root.left = insertIntoBST(root.left,val);
+        }
+        return root;
+    }
+}
