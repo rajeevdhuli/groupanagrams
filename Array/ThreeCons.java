ff19b60916b4c84408c74cc0fd24264e88ce8391
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-19 07:12:38 +0530

    Create ThreeCons.java
    https://leetcode.com/problems/find-three-consecutive-integers-that-sum-to-a-given-number/description/

---
+++ Array/ThreeCons.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long[] sumOfThree(long num) {
+        long[] arr = new long[3];
+        if(num % 3 != 0){
+            return new long[0];
+        }
+        else{
+            
+            arr[1] = (long)num / 3;
+            arr[0] = num/3 - 1;
+            arr[2] = num/3 + 1;
+
+        }
+        return arr;
+        
+    }
+}
