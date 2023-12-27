435337d4c96297f7a8fda252816635afce936114
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-27 11:32:38 +0530

    Create MethodOverriding.java

---
+++ OOPS/MethodOverriding.java
@@ -0,0 +1,23 @@
+public class App {
+    public static class Animal{
+        public void bark(String res){
+            System.out.println(res+" "+"bow");
+        }
+        public void bark(int num){
+            System.out.println((char)num);
+        }
+    }
+    public static class cat extends Animal{
+        public void bark(String res){
+            System.out.println("meow");
+        }
+        public void bark(int num){
+            System.out.println(num/10);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        cat c = new cat();
+        c.bark(66);
+        c.bark("dog");
+    }
+}
