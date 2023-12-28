5f385c7373f405296c986eaeddb6da7e2d6627c9
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-28 15:20:52 +0530

    Create Super_Constructor.java

---
+++ OOPS/Super_Constructor.java
@@ -0,0 +1,28 @@
+class Animal {
+    private String species;
+    public void displaySpecies() {
+        System.out.println("Species: " + species);
+    }
+}
+
+class Dog extends Animal {
+    private String breed;
+
+    public Dog(String breed) {
+        super();
+        this.breed = breed;
+        System.out.println("A dog of breed " + breed + " is created.");
+    }
+
+    public void displayBreed() {
+        System.out.println("Breed: " + breed);
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Labrador");
+        myDog.displaySpecies(); // Output: Species: Canine
+        myDog.displayBreed();  // Output: Breed: Labrador
+    }
+}
