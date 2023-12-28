cc84eecd6f1f4809847b4258bb0d6d7984fd04b1
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2023-12-28 08:21:32 +0530

    Update Absatrct_Class.java

--- OOPS/Absatrct_Class.java
+++ OOPS/Absatrct_Class.java
@@ -1,17 +1,25 @@
 abstract class Shape {
+    //abstract method
     abstract void draw();
+    //concrete method
+    void dance(){
+        System.out.println("dance");
+    }
 }
 
 class Rectangle extends Shape {
+    // sub-class which extends or inherits sbase-class(Shape)
     void draw() {
         System.out.println("drawing rectangle");
     }
 }
 
 class Circle1 extends Shape {
+    //abstract- method should be compulsarly implemented by sub class
     void draw() {
         System.out.println("drawing circle");
     }
+    //Additional method
     void sketch(){
         System.out.println("hi");
     }
@@ -19,12 +27,18 @@ class Circle1 extends Shape {
 
 public class App {
     public static void main(String[] args) throws Exception {
+        //creating an object for circle1
         Circle1 c = new Circle1();
+        //for c all function of shape as well Circle1 can be implemented.
         c.draw();
         c.sketch();
+        c.dance();
         Rectangle r = new Rectangle();
         r.draw();
+        r.dance();
         Shape s = new Circle1();
+        //you cannot put method of circle1 like sketch(), and u cannot instatiatze object for absatrct class shape.
         s.draw();
+        s.dance();
     }
 }
