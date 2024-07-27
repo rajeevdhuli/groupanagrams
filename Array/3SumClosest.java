6c66310d32bd13a223491d1c97fb513553bee977
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-07-27 10:34:53 +0530

    Update 3SumClosest.java
    https://leetcode.com/problems/3sum-closest/description/

--- Array/3SumClosest.java
+++ Array/3SumClosest.java
@@ -1,3 +1,28 @@
+O(N^3) -- BRUTE FORCE ---
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int n = nums.length;
+        int ans = 0;
+        int minDiff = Integer.MAX_VALUE; 
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    int diff = Math.abs(sum - target);
+                    if(minDiff > diff){
+                        ans = sum;
+                        minDiff = diff;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+O(N^2)
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         int n = nums.length;
