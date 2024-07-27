989c6d9f5de0c7852548d02edd6d0071bff0e3da
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-07-27 08:54:38 +0530

    Update 3sum.java
    https://leetcode.com/problems/3sum/description/

--- Array/3sum.java
+++ Array/3sum.java
@@ -31,6 +31,34 @@ public class Main
 	}
 }
 
+
+
+c++ -----------------
+
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        set<vector<int>> set;
+        for(int i = 0; i < n - 2; i++) {
+            for(int j = i + 1; j < n - 1; j++) {
+                for(int k = j + 1; k < n; k++) {
+                    if(nums[i] + nums[j] + nums[k] == 0) {
+                        vector<int> temp = {nums[i], nums[j], nums[k]};
+                        sort(temp.begin(), temp.end());
+                        set.insert(temp);
+                    }
+                }
+            }
+        }
+        vector<vector<int>> list(set.begin(), set.end());
+        return list;
+    }
+};
+	
+
 0(n^3 * nlogn) -
 
 public static void main(String[] args) {
@@ -64,6 +92,38 @@ public static void main(String[] args) {
         }
         System.out.println(ans);
 	}  
+
+
+Alternate Similar Approach - 
+	
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i  = 0 ; i < n - 2; i++){
+            while( i > 0 && i < n - 2 && nums[i] == nums[i-1]){
+                i++;
+            }
+            for(int j = i + 1; j < n - 1; j++){
+                while(j - 1 > i && j < n - 1 && nums[j] == nums[j-1])
+                   j++;
+                for(int k = j + 1; k < n; k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        list.add(temp);
+                        break;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
+
 
 
 O(nlogn + n^2)  -
