89633e3a8bcd2a3d2381f4a3f1f4b6b7e07c3e5a
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-07-27 09:15:18 +0530

    Update 4Sum.java
    https://leetcode.com/problems/4sum/description/

--- Array/4Sum.java
+++ Array/4Sum.java
@@ -31,6 +31,35 @@ class Solution {
     }
 }
 
+c++
+
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        set<vector<int>> set;
+        for(int i = 0 ; i < n - 3; i++){
+            for(int j = i + 1; j < n - 2; j++){
+                for(int k = j + 1; k < n - 1 ; k++){
+                    for(int l = k + 1; l < n ; l++){
+                        if(nums[i] + nums[j] + nums[k] + nums[l] == target){
+                            vector<int> vec;
+                            vec.push_back(nums[i]);
+                            vec.push_back(nums[j]);
+                            vec.push_back(nums[k]);
+                            vec.push_back(nums[l]);
+                            sort(vec.begin(),vec.end());
+                            set.insert(vec);
+                        }
+                    }
+                }
+            }
+        }
+        vector<vector<int>> ans(set.begin(),set.end());
+        return ans;
+    }
+};    
 
 
 
@@ -119,3 +148,53 @@ class Solution {
         return ans;
     }
 }    
+
+
+
+
+Alternate --
+
+
+ class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> vec;
+        for(int i = 0 ; i < n - 3; i++){
+            while( i > 0 && i < n - 3 && nums[i] == nums[i-1])
+                i++;
+            for(int j = i + 1; j < n - 2; j++){
+                while(j < n - 2 && j > i + 1 && nums[j] == nums[j-1])
+                    j++;
+                int left = j + 1;
+                int right = n - 1;
+                while(left < right){
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[left];
+                    sum += nums[right];
+                    /* since sum is large make sure you are adding individually*/
+                    if(sum > target){
+                        right--;
+                    }else if (sum < target){
+                        left++;
+                    }else{
+                        vector<int> temp = {nums[i],nums[j],nums[left],nums[right]};
+                        vec.push_back(temp);
+                        left++;
+                        right--;
+                        while(left < right && nums[left] == nums[left-1]){
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right+1]){
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+        return vec;
+    }
+};   
+    
