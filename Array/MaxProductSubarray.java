9347dcfe2661fa82f05176a6a331d48fc04e6717
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-07-28 12:12:18 +0530

    Update MaxProductSubarray.java
    https://leetcode.com/problems/maximum-product-subarray/

--- Array/MaxProductSubarray.java
+++ Array/MaxProductSubarray.java
@@ -43,3 +43,35 @@ class Solution
        return max;    
     }
 }
+
+
+
+
+
+
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        long  prod = 1;
+        int maxProd = Integer.MIN_VALUE;
+        for(int i = 0 ; i < n ; i++){
+            prod = prod * nums[i];
+            prod = (prod >= Integer.MAX_VALUE) ?  Integer.MAX_VALUE : prod;
+            prod = (prod <= Integer.MIN_VALUE) ?  Integer.MIN_VALUE : prod;
+            maxProd = Math.max(maxProd, (int)prod);
+            if(prod == 0 )
+                prod = 1;      
+        }
+        prod = 1;
+        for(int i = n - 1; i >= 0; i--){
+            prod = prod * nums[i];
+            prod = (prod >= Integer.MAX_VALUE) ?  Integer.MAX_VALUE : prod;
+            prod = (prod <= Integer.MIN_VALUE) ?  Integer.MIN_VALUE : prod;
+            maxProd = Math.max(maxProd, (int)prod);
+            if( prod == 0)
+                prod = 1;     
+        }
+        return maxProd;
+    }
+}
