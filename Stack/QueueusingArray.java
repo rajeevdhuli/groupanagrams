131f94b256edd51a39b9ae9e0fc2fb70a7f47876
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-02 05:39:46 +0530

    Update QueueusingArray.java
    https://www.geeksforgeeks.org/problems/implement-queue-using-array/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=implement-queue-using-array

--- Stack/QueueusingArray.java
+++ Stack/QueueusingArray.java
@@ -57,3 +57,56 @@ class MyQueue {
 	    return false;     
 	}
 }
+
+
+
+
+
+
+
+class MyQueue {
+    int front, rear;
+	int arr[] = new int[100005];
+	int cursize;
+    MyQueue()
+	{
+		front=0;
+		rear=0;
+		cursize =0;
+	}
+	//Function to push an element x in a queue.
+	void push(int x)
+	{
+	    // Your code here
+	    if(isFull())
+	        return;
+	    arr[rear] = x;
+	    rear = (rear < 100005 - 1) ? rear + 1 : 0;
+	    cursize++;
+	} 
+    //Function to pop an element from queue and return that element.
+	int pop()
+	{
+		// Your code here
+		if(isEmpty()){
+		    return -1;
+		}
+		int x = arr[front];
+		front = (front < 100005 - 1) ? front + 1 : 0;
+		cursize--;
+		return x;
+	} 
+	boolean isEmpty(){
+	    if(cursize == 0){
+	        return true;
+	    }
+	    return false;
+	}
+	boolean isFull(){
+	    if(cursize == 100005){
+	        return true;
+	    }
+	    return false;
+	}
+}
+
