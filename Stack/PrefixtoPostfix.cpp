0c8098cfcc09cb446da189c7d9a8025567059a87
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-03 14:45:19 +0530

    Update PrefixtoPostfix.cpp
    https://www.geeksforgeeks.org/problems/prefix-to-postfix-conversion/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=prefix-to-postfix-conversion

--- Stack/PrefixtoPostfix.cpp
+++ Stack/PrefixtoPostfix.cpp
@@ -24,3 +24,41 @@ string preToPost(string s) {
     }
     res = st.top();
     return res;
+
+
+
+
+
+
+JAVA - 
+
+
+class Solution {
+    static String preToPost(String pre_exp) {
+        // code here
+                Stack<String> st = new Stack<>();
+        int n = pre_exp.length();
+
+        // Traverse the prefix expression from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = pre_exp.charAt(i);
+
+            if (Character.isLetterOrDigit(ch)) {
+                // Push operands (numbers or variables) onto the stack
+                st.push(String.valueOf(ch));
+            } else {
+                // Pop two operands from the stack
+                String op1 = st.pop();
+                String op2 = st.pop();
+
+                // Create the postfix expression and push it back to the stack
+                String expr = op1 + op2 + ch;
+                st.push(expr);
+            }
+        }
+
+        // The final element of the stack is the postfix expression
+        return st.pop();
+    }
+}
+
