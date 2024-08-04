bea636eef280b0b51c1476669533b93e2f845c4a
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-04 13:06:28 +0530

    Update NextGreaterElement.java
    https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1

--- Stack/MonotonicStack/NextGreaterElement.java
+++ Stack/MonotonicStack/NextGreaterElement.java
@@ -17,3 +17,23 @@ class Solution
         
     } 
 }
+
+
+
+#include <stack>
+vector<int> nextGreaterElement(vector<int>& arr, int n)
+{
+	// Write your code here
+    vector<int> v;
+    stack<int> st;
+    for(int i = n-1; i >= 0; i--)
+    {
+        while((!st.empty()) && st.top() <= arr[i])
+            st.pop();
+        int prev = (st.empty()) ? (-1) : (st.top());
+        v.push_back(prev);
+        st.push(arr[i]);
+    }
+    reverse(v.begin(),v.end());
+    return v;
+}
