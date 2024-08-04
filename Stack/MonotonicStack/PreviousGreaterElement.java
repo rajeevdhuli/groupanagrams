72c32da503236e60de47452e101a6b733c4bd1dd
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-04 12:33:26 +0530

    Create PreviousGreaterElement.java
    https://www.geeksforgeeks.org/batch/cpp-stl-1/track/CPP-STL-Container-Adapters/video/MTU4NA%3D%3D

---
+++ Stack/MonotonicStack/PreviousGreaterElement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] prevGreaterElement(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < n ; i++{
+            while(!st.isEmpty() && nums[i] >= st.peek()){
+                st.pop();
+            }
+            ans[i] =  (st.isEmpty()) ? -1 : st.peek();
+            st.push(nums[i]);
+        }
+        return ans;
+    }
+}
