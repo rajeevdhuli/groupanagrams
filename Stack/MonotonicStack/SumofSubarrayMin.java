b3250a7791e891bb0fd646827c43fd19cab70f84
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-08 06:01:33 +0530

    Update SumofSubarrayMin.java
    https://leetcode.com/problems/sum-of-subarray-minimums/description/

--- Stack/MonotonicStack/SumofSubarrayMin.java
+++ Stack/MonotonicStack/SumofSubarrayMin.java
@@ -13,17 +13,17 @@ class Solution {
         }
         st.clear();
         for(int i  =  n - 1 ; i >= 0 ; i--){
-            while(!st.isEmpty() && arr[i] <= arr[st.peek()]){
+            while(!st.isEmpty() && arr[i] < arr[st.peek()]){
                 st.pop();
             }
             nextSmall[i] = (st.isEmpty()) ?  n : (st.peek());
             st.push(i);
         }
-        int res = 0 ;
+        long res = 0 ;
         for(int i = 0 ; i  < n ; i++){
             int left = (i - prevSmall[i]);
             int right = (nextSmall[i] - i);
-            res+= (long)(left * right *  arr[i]);
+            res+= (long)(left * right) *  arr[i];
             res = res % 1000000007;
         }
         return (int) res;
