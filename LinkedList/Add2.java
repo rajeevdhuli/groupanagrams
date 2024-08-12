0db5527dc995dd5833f723a3837a893bb2034744
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-12 21:55:49 +0530

    Update Add2Numbers.java
    https://leetcode.com/problems/add-two-numbers/

--- LinkedList/Add2.java
+++ LinkedList/Add2.java
@@ -31,3 +31,34 @@ class Solution {
         return ans.next;
     }
 }
+
+
+
+Optimized - 
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(-1);
+        ListNode head = ans;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry == 1){
+            int sum = 0 ;
+            sum+= carry;
+            if(l1 != null){
+                sum+= l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum+= l2.val;
+                l2 = l2.next;
+            }
+            // carry cannot be more than1 beacuse max case 9+9+1 = 19
+            carry = (sum < 10) ? 0 : 1;
+            sum = (sum < 10) ? sum : (sum % 10);
+            ListNode temp = new ListNode(sum);
+            head.next = temp;
+            head = head.next;
+        }
+        return ans.next;
+    }
+}
