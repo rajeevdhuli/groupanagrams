8da05efbbb540f83559d530d0a1901fa1a43a5d9
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-13 07:04:42 +0530

    Update RemoveDuplicates.java
    https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/

--- LinkedList/RemoveDuplicates.java
+++ LinkedList/RemoveDuplicates.java
@@ -1,17 +1,39 @@
-public static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode llist) {
-    // Write your code here
-    SinglyLinkedListNode cur = llist;
-    while(cur.next !=null)
-    {
-        SinglyLinkedListNode temp = cur.next;
-        if(temp.data == cur.data)
-        {
-            cur.next = temp.next;
+O(N) , O(N) --
+class Solution {
+    public ListNode deleteDuplicates(ListNode llist) {
+     Set<Integer> set = new HashSet<>();
+    ListNode dummy = new ListNode(-1);
+    ListNode head = dummy;
+    while(llist != null){
+        if(set.contains(llist.val)){
+            llist = llist.next;
+        }else{
+        set.add(llist.val);
+        head.next = llist;
+        llist = llist.next;
+        head = head.next;
         }
-        else 
-           cur = cur.next;
     }
-    return llist;
+    head.next = null;
+    return dummy.next;
     }
+}
 
+O(N),O(1)---
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next; // Skip the duplicate node
+            } else {
+                current = current.next; // Move to the next distinct node
+            }
+        }
+
+        return head;
+    }
 }
