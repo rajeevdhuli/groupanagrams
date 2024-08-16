f89dc681fe9e3e76120a1dacbeb9d1039f2c90a2
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-16 11:09:29 +0530

    Update DeleteallDLL.java
    https://www.geeksforgeeks.org/problems/delete-all-occurrences-of-a-given-key-in-a-doubly-linked-list/1

--- LinkedList/DeleteallDLL.java
+++ LinkedList/DeleteallDLL.java
@@ -2,57 +2,58 @@ class Solution {
     static Node deleteAllOccurOfX(Node head, int x) {
         // Write your code here
         Node cur = head;
-        Node pre = null;
         if(head == null)
            return null;
-        if(head.next == null && head.data == x)
-            return null;
         while(cur != null){
-            
-            while(cur.data == x && cur.next != null && cur != head){
-                pre.next = cur.next;
-                cur.next.prev = pre;
-                cur = cur.next;
-            }
-            
-            if (cur.data == x && cur.next == null && cur != head){
-                pre.next = cur.next;
+            Node nextNode = cur.next;
+            //remove head of a dll
+            if(cur.data == x && cur == head){
+                head = head.next;
                 cur.prev = null;
-                // pre = cur.next;
-                // cur = cur.next;
-                break;
+            }//remove head of a dll if it contains only one element
+            else if (cur.data == x && cur.next == null && cur == head){
+                head = null;
+            }//remove the middle ones
+            else if(cur.data == x && cur.next != null && cur != head){
+                cur.prev.next = cur.next;
+                cur.next.prev = cur.prev;
+            }//remove the last element of dll
+            else if(cur.data == x && cur.next == null && cur != head){
+                cur.prev.next = cur.next;
             }
-            else if (cur.data == x &&  cur == head){
-                head = cur.next;
-                cur = head;
+            //clean up the garbage
+            if(cur.data == x){
                 cur.prev = null;
+                cur.next = null;
             }
-            else{
-                pre = cur;
-                cur = cur.next;
-            }
+            //move cur to next 
+            //u can cur = cur.next, if u were not clenaing up
+            cur = nextNode;
         }
         return head;
     }
 }
 
-
-
 class Solution {
     static Node deleteAllOccurOfX(Node head, int x) {
         // Write your code here
+        if(head == null){
+            return null;
+        }
         Node cur = head;
         while(cur != null){
             if(cur.data == x){
                 if(cur.prev != null){
                     cur.prev.next = cur.next;
-                }
-                else{
-                    head = cur.next;
+                }else{
+                    head = cur.next;//update first node if deleting the first node
                 }
                 if(cur.next != null){
                     cur.next.prev = cur.prev;
                 }
+                //clean the node being deleted
+                cur.next = null;
+                cur.prev = null;
             }
             cur = cur.next;
         }
