a01dfe69c7d1304d393b0282d3fc7e19ed65602b
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-16 08:25:11 +0530

    Update DeleteNodeDLL.java
    https://www.geeksforgeeks.org/problems/delete-node-in-doubly-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=delete-node-in-doubly-linked-list

--- LinkedList/DeleteNodeDLL.java
+++ LinkedList/DeleteNodeDLL.java
@@ -1,36 +1,49 @@
-class Solution
-{
-    // function returns the head of the linkedlist
-    Node deleteNode(Node head,int x)
-    {
-	// Your code here
-	
-	int i = 1;
-	Node cur = head;
-	Node pre = null;
-	
-	while(i < x){
-	    pre = cur;
-	    cur = cur.next;
-	    i++;
-	}
-	
-	if(pre == null){
-	    Node temp = head.next;
-	    temp.prev = null;
-	    return temp;
-	}
-	
-	Node temp = cur.next;
-	if(temp == null){
-	    pre.next = temp;
-	    cur.prev = null;
-	    return head;
-	}
-	pre.next = cur.next;
-	temp.prev = cur.prev;
-	cur.prev = null;
-	cur.next = null;
-	return head;
+public class Solution {
+    public Node deleteNode(Node head, int x) {
+        // Edge case: if the list is empty
+        if (head == null) {
+            return null;
+        }
+
+        // Case 1: Deleting the head node
+        if (x == 1) {
+            Node newHead = head.next; // New head is the next node
+            if (newHead != null) {
+                newHead.prev = null; // Update the new head's prev pointer
+            }
+            // Clean up the old head
+            head.next = null;
+            head.prev = null;
+            return newHead; // Return the new head of the list
+        }
+
+        // Case 2: Traverse to the node before the one we want to delete
+        Node current = head;
+        int i = 1;
+        while (i < x - 1 && current != null) {
+            current = current.next;
+            i++;
+        }
+
+        // If the node to be deleted is beyond the end of the list
+        if (current == null || current.next == null) {
+            return head;
+        }
+
+        // Node to be deleted
+        Node nodeToDelete = current.next;
+        Node nextNode = nodeToDelete.next;
+
+        // Update links to skip over the node to be deleted
+        current.next = nextNode;
+        if (nextNode != null) {
+            nextNode.prev = current;
+        }
+
+        // Clean up the deleted node
+        nodeToDelete.next = null;
+        nodeToDelete.prev = null;
+
+        return head;
     }
 }
