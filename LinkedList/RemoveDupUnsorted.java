ee87fc8e108a485a5b0dd0c3b83142a8648809ca
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-16 13:14:05 +0530

    Update RemoveDupUnsorted.java
    https://www.geeksforgeeks.org/problems/remove-duplicates-from-an-unsorted-linked-list/1?page=1&category=Linked%2520List&sortBy=submissions

--- LinkedList/RemoveDupUnsorted.java
+++ LinkedList/RemoveDupUnsorted.java
@@ -1,66 +1,63 @@
-JAVA - 
-
-class Solution
-{
-    //Function to remove duplicates from unsorted linked list.
-    public Node removeDuplicates(Node head) 
-    {
-         // Your code here
-        Map<Integer,Integer> mp = new HashMap<>();
-        Node cur = head;
-        while(cur != null){
-            mp.put(cur.data,1);
-            cur = cur.next;
-        }
-        cur = head;
-        Node prev = null;
-        Node right = head;
-        while(right != null){
-            if(mp.containsKey(right.data)){
-                mp.remove(right.data);
-                cur.data = right.data;
-                prev = cur;
-                cur = cur.next;
-            }
-            right = right.next;
-        }
-        prev.next = null;
-        return head;
-    }
-}
 
 
 
 C++ -
 
-class Solution
-{
-    public:
-    //Function to remove duplicates from unsorted linked list.
-    Node * removeDuplicates( Node *head) 
-    {
-     // your code goes here
-     Node* cur= head;
+class Solution {
+  public:
+    Node *removeDuplicates(Node *head) {
+        // your code goes here
+        Node* cur= head;
      unordered_map<int,int> mp;
-     while(cur!= NULL){
-         mp[cur->data] = 1;
+     while(cur!= nullptr){
+         mp[cur->data]++;
          cur = cur->next;
      }
-     cur = head;
-     Node* right = head;
-     Node* prev = NULL;
-     while(right != NULL){
-         if(mp.count(right->data))
-         {
-             mp.erase(right->data);
-             cur->data = right->data;
-             prev = cur;
-             cur = cur->next;
+     Node dummy(-1);
+     Node* pre = &dummy;
+     while(head != nullptr){
+         if(mp.count(head->data) > 0 && mp[head->data] > 1){
+             //keep first one and delete
+             pre->next = head;
+             pre = pre->next;
+             mp.erase(head->data);
+         }else if(mp.count(head->data) == 1){
+             //keep ones whihc are occurin one time
+             pre->next = head;
+             pre = pre->next;
          }
-         right = right->next;
+         head = head->next; 
      }
-     prev->next = NULL;
-     return head;
-     
+     pre->next = nullptr;
+     return dummy.next;
     }
 };
+
+
+
+
+shorter - optimized
+
+public Node removeDuplicates(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Set<Integer> seen = new HashSet<>();
+        Node cur = head;
+        Node prev = null;
+
+        while (cur != null) {
+            if (seen.contains(cur.data)) {
+                // If the value has already been seen, skip the current node
+                prev.next = cur.next;
+            } else {
+                // If it's the first occurrence, add it to the set and move prev forward
+                seen.add(cur.data);
+                prev = cur;
+            }
+            cur = cur.next;
+        }
+
+        return head;
+    }
