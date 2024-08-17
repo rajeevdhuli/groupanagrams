8af8980e46fdc275baa31f7b6df55d35e2e2470a
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-17 16:28:28 +0530

    Create DeletePresentinArray.java
    https://leetcode.com/problems/delete-nodes-from-linked-list-present-in-array/

---
+++ LinkedList/AdditionalLeetcode/DeletePresentinArray.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums){
+            set.add(i);
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while(head != null){
+            if(!set.contains(head.val)){
+                cur.next = head;
+                cur = cur.next;
+            }
+            head = head.next;
+        }
+        cur.next = null;
+        return dummy.next;
+    }
+}
