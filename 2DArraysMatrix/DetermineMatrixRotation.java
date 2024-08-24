baf4765fa3d364b484a9b55e467d8b5a061990b2
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-24 08:56:18 +0530

    Update DetermineMatrixRotation.java
    https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/description/

--- 2DArraysMatrix/DetermineMatrixRotation.java
+++ 2DArraysMatrix/DetermineMatrixRotation.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
         int n = mat.length;
-        boolean p = true, q = true, r = true, s = true;
-        for(int i =0; i < n; i++){
-            for(int j = 0 ; j< n; j++){
+        boolean rot90 = true, rot180 = true, rot270 = true, rot360 = true;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
                 if(mat[i][j] != target[j][n-i-1])
-                   p = false;
+                    rot90 = false;
                 if(mat[i][j] != target[n-i-1][n-j-1])
-                   q = false;
+                    rot180 = false;
                 if(mat[i][j] != target[n-j-1][i])
-                   r = false; 
+                    rot270 = false;
                 if(mat[i][j] != target[i][j])
-                   s = false;        
+                   rot360 = false;            
             }
         }
-        return ( p || q || r || s);
+        return (rot90 || rot180 || rot270 || rot360);
     }
 }
