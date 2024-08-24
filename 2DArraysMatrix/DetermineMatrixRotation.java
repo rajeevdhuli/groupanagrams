2e5dd34e2ca2f2592d13d694543ca378fd58bdb5
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-24 09:00:45 +0530

    Update DetermineMatrixRotation.java
    https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/description/

--- 2DArraysMatrix/DetermineMatrixRotation.java
+++ 2DArraysMatrix/DetermineMatrixRotation.java
@@ -17,3 +17,58 @@ class Solution {
         return (rot90 || rot180 || rot270 || rot360);
     }
 }
+
+
+Alternate by transposing and rotating 
+
+
+
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
+
+        // Try rotating the matrix 0, 90, 180, and 270 degrees
+        for (int r = 0; r < 4; r++) {
+            if (checkEqual(mat, target)) {
+                return true;
+            }
+            rotate90(mat);
+        }
+        
+        return false;
+    }
+
+    // Function to rotate the matrix by 90 degrees clockwise
+    private void rotate90(int[][] mat) {
+        int n = mat.length;
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0, k = n - 1; j < k; j++, k--) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][k];
+                mat[i][k] = temp;
+            }
+        }
+    }
+
+    // Function to check if two matrices are equal
+    private boolean checkEqual(int[][] mat, int[][] target) {
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != target[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
