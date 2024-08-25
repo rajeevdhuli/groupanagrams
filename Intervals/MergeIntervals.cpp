82f3553717fbc39872bd597c179567a59918b7a9
rajeevdhuli <144231948+rajeevdhuli@users.noreply.github.com>
2024-08-25 12:16:18 +0530

    Update MergeIntervals.cpp
    https://leetcode.com/problems/merge-intervals/description/

--- Intervals/MergeIntervals.cpp
+++ Intervals/MergeIntervals.cpp
@@ -68,25 +68,60 @@ public:
 };
 
 
+OPTIMIZED ---------
 
+JAVA -
+Time Complexity: O(N log N) + O(N) 
+Space Complexity: O(N)    
+    
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        //comparator sorting - compares based on first value
+        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
+        List<List<Integer>> list = new ArrayList<>();
+        int n = intervals.length;
+        for(int i = 0 ; i < n; i++){
+            if(list.isEmpty() || list.get(list.size()-1).get(1) < intervals[i][0]){
+                List<Integer> pair = new ArrayList<>();
+                pair.add(intervals[i][0]);
+                pair.add(intervals[i][1]);
+                list.add(pair);
+            }else {
+                list.get(list.size()-1).set(1,Math.max(list.get(list.size()-1).get(1),intervals[i][1]));
+            }
+        }
+        // int colSize = (list.isEmpty()) ? 0 : list.get(0).size();
+        // you can just insert 2 since it cannot be more than 2
+        int[][] ans = new int[list.size()][2];
+        for(int i = 0; i < list.size(); i++){
+            // List<Integer> temp = list.get(i);
+            // ans[i][0] = temp.get(0);
+            // ans[i][1] = temp.get(1);
+            ans[i][0] = list.get(i).get(0);
+            ans[i][1] = list.get(i).get(1);
+        }
+        return ans;
+    }
+}    
 
-
-O(NLOGN) + O(N) - 
-
+C++
+Time Complexity: O(N log N) + O(N) 
+Space Complexity: O(N) 
+    
 
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
-        vector<vector<int>> ans;
-        int n = intervals.size();
-        for(int i = 0; i < n; i++){
-            if(ans.empty() ||  ans[ans.size()-1][1] < intervals[i][0]){
-                ans.push_back(intervals[i]);
-            }else {
-                ans[ans.size()-1][1] = max(intervals[i][1],ans[ans.size()-1][1]);
+        vector<vector<int>> vec ;
+        for(int i = 0 ; i < intervals.size(); i++){
+            if(vec.empty() || vec[vec.size()-1][1] < intervals[i][0]){
+                vec.push_back({intervals[i][0],intervals[i][1]});
+            }else{
+                //vec.back()[1] = max(vec.back()[1],intervals[i][1]);
+                vec[vec.size()-1][1] = max(vec[vec.size()-1][1],intervals[i][1]);
             }
         }
-        return ans;
+        return vec;
     }
-};    
+};
